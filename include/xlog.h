fd20f61c995c1462740f8b674c388a5987a610c2
Dmitriy Kalugin-Balashov <rvncerr@rvncerr.org>
2026-03-28 04:06:20 +0000

    Add symbol visibility control to shared library

--- include/xlog.h
+++ include/xlog.h
@@ -15,6 +15,12 @@
 #define XLOG_SKIP_CORRUPT (1 << 1)
 #define XLOG_SKIP_BADSIZE (1 << 2)
 
+#ifdef XLOG_BUILDING
+    #define XLOG_API __attribute__((visibility("default")))
+#else
+    #define XLOG_API
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -22,16 +28,16 @@ extern "C" {
 typedef struct xlog_reader xlog_reader;
 typedef struct xlog_writer xlog_writer;
 
-xlog_reader *xlog_reader_open(const char *path);
-xlog_reader *xlog_reader_open_ex(const char *path, uint32_t max_record_size, int flags);
-void xlog_reader_reset(xlog_reader *r);
-ssize_t xlog_reader_next(xlog_reader *r, void **buf);
-void xlog_reader_close(xlog_reader *r);
+XLOG_API xlog_reader *xlog_reader_open(const char *path);
+XLOG_API xlog_reader *xlog_reader_open_ex(const char *path, uint32_t max_record_size, int flags);
+XLOG_API void xlog_reader_reset(xlog_reader *r);
+XLOG_API ssize_t xlog_reader_next(xlog_reader *r, void **buf);
+XLOG_API void xlog_reader_close(xlog_reader *r);
 
-xlog_writer *xlog_writer_open(const char *path);
-xlog_writer *xlog_writer_open_ex(const char *path, uint32_t max_record_size, int flags);
-int xlog_writer_commit(xlog_writer *w, const void *buf, size_t sz);
-void xlog_writer_close(xlog_writer *w);
+XLOG_API xlog_writer *xlog_writer_open(const char *path);
+XLOG_API xlog_writer *xlog_writer_open_ex(const char *path, uint32_t max_record_size, int flags);
+XLOG_API int xlog_writer_commit(xlog_writer *w, const void *buf, size_t sz);
+XLOG_API void xlog_writer_close(xlog_writer *w);
 
 #ifdef __cplusplus
 }
--- src/crc32c.h
+++ src/crc32c.h
@@ -1,9 +1,10 @@
 #ifndef XLOG_CRC32C_H
 #define XLOG_CRC32C_H
 
+#include "xlog.h"
 #include <stdint.h>
 #include <stddef.h>
 
-uint32_t crc32c(uint32_t crc, const void *buf, size_t size);
+XLOG_API uint32_t crc32c(uint32_t crc, const void *buf, size_t size);
 
 #endif //XLOG_CRC32C_H
