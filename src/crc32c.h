c196ab52e8ed45c57ed46733ef5ebf01cc8e247c
Dmitriy Kalugin-Balashov <rvncerr@rvncerr.org>
2026-04-03 22:27:30 -0400

    Hide crc32c from the public ABI
    Remove XLOG_API from crc32c.h so crc32c() is no longer exported from
    the shared library. Compile crc32c.c directly into the test binary
    since it needs the symbol for CRC unit tests.

--- src/crc32c.h
+++ src/crc32c.h
@@ -1,10 +1,9 @@
 #ifndef XLOG_CRC32C_H
 #define XLOG_CRC32C_H
 
-#include "xlog.h"
 #include <stdint.h>
 #include <stddef.h>
 
-XLOG_API uint32_t crc32c(uint32_t crc, const void *buf, size_t size);
+uint32_t crc32c(uint32_t crc, const void *buf, size_t size);
 
 #endif //XLOG_CRC32C_H
